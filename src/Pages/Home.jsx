3f3f23120b5c120dbd3ce40ce6623efb2053c4f3
SamMidnight03 <ssgomezeliz@gmail.com>
2025-02-20 18:50:43 -0500

    five commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main>
             <section id="InfoTaskCenter">
-                <img src="/public/task_center-logo.png" alt="Logo Task center" />
+                <img src="/task_center-logo.png" alt="Logo Task center" />
                 <div>
                     <h1>¿Quienes somos?</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo numquam sapiente consequuntur. Dignissimos animi et enim voluptates aspernatur provident labore facere molestias esse ducimus? Quos quibusdam molestiae sapiente dicta minima placeat eius tempora? Quis autem eligendi unde quia fuga veritatis, illum error sunt, sequi quidem accusamus. Deserunt, provident ad aut, maxime dolore totam quasi fuga consectetur excepturi voluptates error, voluptatem nostrum! Quia quam, voluptatibus quos incidunt minus deleniti quo mollitia maxime neque repellendus consectetur, aspernatur voluptates praesentium ratione officia labore placeat optio atque qui eligendi molestias fuga molestiae. Dolores magnam voluptatem porro corporis enim praesentium fugiat dicta sequi aspernatur deleniti.</p>
